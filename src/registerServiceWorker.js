f00eb98fc1264b7f26c34707ce8cdd5d812e140c
jeonghun94 <jeoongh1021@naver.com>
2023-10-31 18:02:54 +0900

    원복

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,10 +1,10 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("./assets/sw.js")
+    .register("/sw.js")
     .then(function (registration) {
-      console.log("Service Worker registered with scope:", registration.scope);
+      console.log("Service Worker connect succese", registration.scope);
     })
     .catch(function (err) {
-      console.log("Service Worker registration failed", err);
+      console.log("Service Worker connect fail", err);
     });
 }
