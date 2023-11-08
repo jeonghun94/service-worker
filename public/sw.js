5178fc65afa93d1c85d5ebc25f638014e4f5b0a7
jeonghun94 <jeongh1021@naver.com>
2023-11-08 17:52:30 +0900

    sw.js promise -> async/await 형식 변경

--- public/sw.js
+++ public/sw.js
@@ -34,66 +34,117 @@ self.addEventListener('fetch', (event) => {
   }
 });
 
-const cacheResource = async (cache, url) => {
+// const cacheResource = async (cache, url) => {
+//   try {
+//     const cacheResponse = await cache.match(url);
+//     if (!cacheResponse) {
+//       const networkResponse = await fetch(url);
+//       cache.put(url, networkResponse.clone());
+//     }
+//   } catch (error) {
+//     console.error(`캐싱 중 오류 발생: ${error}`);
+//   }
+// };
+
+// self.addEventListener('fetch', function (event) {
+//   caches.open('my-cache').then((cache) => {
+//     cache.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request).then((networkResponse) => {
+//           if (event.request.url.includes('/api')) {
+//             return networkResponse.json().then((data) => {
+//               const cacheResponse = new Response(JSON.stringify(data), {
+//                 headers: {
+//                   'Content-Type': 'application/json; charset=utf-8',
+//                 },
+//               });
+
+//               data.forEach((item) => {
+//                 if (item.contents && item.contents.videos) {
+//                   item.contents.videos.forEach(async (video) => {
+//                     await cacheResource(cache, video);
+//                   });
+//                 }
+
+//                 if (item.contents && item.contents.images) {
+//                   item.contents.images.forEach(async (image) => {
+//                     await cacheResource(cache, image);
+//                   });
+//                 }
+
+//                 if (item.contents && item.contents.pdf) {
+//                   item.contents.pdf.forEach(async (audio) => {
+//                     await cacheResource(cache, audio);
+//                   });
+//                 }
+//               });
+
+//               const url = new URL(event.request.url);
+//               const path = url.pathname + url.search;
+
+//               cache.put(`api-data-${path}`, cacheResponse);
+//             });
+//           } else {
+//             cache.put(event.request, networkResponse.clone());
+//             return networkResponse;
+//           }
+//         });
+//       }
+//     });
+//   });
+// });
+
+const cacheResource = async (cache, resource) => {
   try {
-    const cacheResponse = await cache.match(url);
-    if (!cacheResponse) {
-      const networkResponse = await fetch(url);
-      cache.put(url, networkResponse.clone());
-    }
+    const response = await fetch(resource);
+    await cache.put(resource, response.clone());
   } catch (error) {
-    console.error(`캐싱 중 오류 발생: ${error}`);
+    // 처리하거나 로깅할 수 있음
   }
 };
 
-self.addEventListener('fetch', function (event) {
-  caches.open('my-cache').then((cache) => {
-    cache.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then((networkResponse) => {
-          if (event.request.url.includes('/api')) {
-            return networkResponse.json().then((data) => {
-              const cacheResponse = new Response(JSON.stringify(data), {
-                headers: {
-                  'Content-Type': 'application/json; charset=utf-8',
-                },
-              });
+self.addEventListener('fetch', async (event) => {
+  const cache = await caches.open('my-cache');
+  const cachedResponse = await cache.match(event.request);
 
-              data.forEach((item) => {
-                if (item.contents && item.contents.videos) {
-                  item.contents.videos.forEach(async (video) => {
-                    await cacheResource(cache, video);
-                  });
-                }
+  if (cachedResponse) {
+    return cachedResponse;
+  }
 
-                if (item.contents && item.contents.images) {
-                  item.contents.images.forEach(async (image) => {
-                    await cacheResource(cache, image);
-                  });
-                }
+  const networkResponse = await fetch(event.request);
 
-                if (item.contents && item.contents.pdf) {
-                  item.contents.pdf.forEach(async (audio) => {
-                    await cacheResource(cache, audio);
-                  });
-                }
-              });
-
-              const url = new URL(event.request.url);
-              const path = url.pathname + url.search;
-
-              cache.put(`api-data-${path}`, cacheResponse);
-            });
-          } else {
-            cache.put(event.request, networkResponse.clone());
-            return networkResponse;
-          }
-        });
-      }
+  if (event.request.url.includes('/api')) {
+    const data = await networkResponse.json();
+    const cacheResponse = new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json; charset=utf-8',
+      },
     });
-  });
+
+    const url = new URL(event.request.url);
+    const path = url.pathname + url.search;
+
+    await cache.put(`api-data-${path}`, cacheResponse);
+
+    for (const item of data) {
+      if (item.contents) {
+        for (const content of [
+          ...(item.contents.videos || []),
+          ...(item.contents.images || []),
+          ...(item.contents.pdf || []),
+        ]) {
+          await cacheResource(cache, content);
+        }
+      }
+    }
+
+    return cacheResponse;
+  } else {
+    await cache.put(event.request, networkResponse.clone());
+    return networkResponse;
+  }
 });
 
 self.addEventListener('message', (event) => {
