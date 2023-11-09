40e7baec94b0ec10d9e68d97ea7fddfee875630b
jeonghun94 <jeongh1021@naver.com>
2023-11-09 09:30:23 +0900

    offline 관련 sw 로직 수정

--- public/sw.js
+++ public/sw.js
@@ -15,87 +15,6 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  const imageAudioVideoRegex =
-    /\.(png|jpe?g|gif|svg|mp[34]|webm|ogg|wav|flac|aac|wma|m4a|opus|pdf)$/i;
-
-  if (imageAudioVideoRegex.test(event.request.url)) {
-    event.respondWith(
-      caches.open('my-cache').then((cache) => {
-        return cache.match(event.request.url).then((response) => {
-          console.log(
-            '캐싱된 이미지, 오디오, 비디오 파일 요청:',
-            event.request.url,
-          );
-          return response;
-        });
-      }),
-    );
-  }
-});
-
-// const cacheResource = async (cache, url) => {
-//   try {
-//     const cacheResponse = await cache.match(url);
-//     if (!cacheResponse) {
-//       const networkResponse = await fetch(url);
-//       cache.put(url, networkResponse.clone());
-//     }
-//   } catch (error) {
-//     console.error(`캐싱 중 오류 발생: ${error}`);
-//   }
-// };
-
-// self.addEventListener('fetch', function (event) {
-//   caches.open('my-cache').then((cache) => {
-//     cache.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       } else {
-//         return fetch(event.request).then((networkResponse) => {
-//           if (event.request.url.includes('/api')) {
-//             return networkResponse.json().then((data) => {
-//               const cacheResponse = new Response(JSON.stringify(data), {
-//                 headers: {
-//                   'Content-Type': 'application/json; charset=utf-8',
-//                 },
-//               });
-
-//               data.forEach((item) => {
-//                 if (item.contents && item.contents.videos) {
-//                   item.contents.videos.forEach(async (video) => {
-//                     await cacheResource(cache, video);
-//                   });
-//                 }
-
-//                 if (item.contents && item.contents.images) {
-//                   item.contents.images.forEach(async (image) => {
-//                     await cacheResource(cache, image);
-//                   });
-//                 }
-
-//                 if (item.contents && item.contents.pdf) {
-//                   item.contents.pdf.forEach(async (audio) => {
-//                     await cacheResource(cache, audio);
-//                   });
-//                 }
-//               });
-
-//               const url = new URL(event.request.url);
-//               const path = url.pathname + url.search;
-
-//               cache.put(`api-data-${path}`, cacheResponse);
-//             });
-//           } else {
-//             cache.put(event.request, networkResponse.clone());
-//             return networkResponse;
-//           }
-//         });
-//       }
-//     });
-//   });
-// });
-
 const cacheResource = async (cache, resource) => {
   try {
     const response = await fetch(resource);
@@ -144,6 +63,33 @@ self.addEventListener('fetch', async (event) => {
   } else {
     await cache.put(event.request, networkResponse.clone());
     return networkResponse;
+  }
+});
+
+self.addEventListener('fetch', async (event) => {
+  event.respondWith(
+    caches.open('my-cache').then((cache) => {
+      return cache.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      });
+    }),
+  );
+
+  const imageAudioVideoRegex =
+    /\.(png|jpe?g|gif|svg|mp[34]|webm|ogg|wav|flac|aac|wma|m4a|opus|pdf)$/i;
+
+  if (imageAudioVideoRegex.test(event.request.url)) {
+    event.respondWith(
+      caches.open('my-cache').then((cache) => {
+        return cache.match(event.request.url).then((response) => {
+          console.log(
+            '캐싱된 이미지, 오디오, 비디오 파일 요청:',
+            event.request.url,
+          );
+          return response;
+        });
+      }),
+    );
   }
 });
 
