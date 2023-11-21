010b148fb4c9a21769804f06fc6e9cec99b0b542
jeonghun94 <jeongh1021@naver.com>
2023-11-21 11:00:33 +0900

    강의 전일, 오늘 ,내일 단순 목록 분리

--- src/components/course/CourseList.vue
+++ src/components/course/CourseList.vue
@@ -2,6 +2,38 @@
   <NavBar />
   <div class="mt-8">
     <div
+      v-for="(group, title) in filteredClassInfoData"
+      :key="title"
+      class="mb-5"
+    >
+      <h2 class="text-lg font-bold text-left">{{ title }}</h2>
+      <div
+        v-for="(item, index) in group"
+        :key="index"
+        class="flex justify-between items-center text-xs border rounded-md p-3 w-full my-3"
+      >
+        <router-link :to="'/course/' + item.courseCode" class="text-black">
+          <div class="flex flex-col gap-3">
+            <h1 class="font-semibold text-[1.5rem]">
+              {{ item.courseName }}
+            </h1>
+            <p class="flex gap-1">
+              <span>{{ item.instructorName }}</span>
+              <span>/</span>
+              <span>{{ item.startDate }}</span>
+            </p>
+          </div>
+        </router-link>
+        <img
+          class="w-14 h-14 rounded-md"
+          :src="item.courseThumbnail"
+          alt="logo"
+        />
+      </div>
+    </div>
+  </div>
+  <!-- <div class="mt-8">
+    <div
       class="flex justify-between items-center text-xs border rounded-md p-3 m-2 w-full"
       v-for="(item, index) in classInfoData"
       :key="index"
@@ -24,12 +56,12 @@
         alt="logo"
       />
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
 import axios from 'axios';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { URL } from '../../constants';
 import NavBar from '../NavBar.vue';
 
@@ -66,13 +98,46 @@ export default {
 
     onMounted(async () => {
       await fetchData();
+
       navigator.serviceWorker.addEventListener('message', async (event) => {
         const { cachedData } = await JSON.parse(event.data);
         classInfoData.value = cachedData;
       });
     });
 
+    const filteredClassInfoData = computed(() => {
+      const today = new Date().toISOString().split('T')[0];
+
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+      const yesterdayString = yesterday.toISOString().split('T')[0];
+
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      const tomorrowString = tomorrow.toISOString().split('T')[0];
+
+      const groupedData = {
+        '전일 수강 목록': [],
+        '오늘 수강 목록': [],
+        '내일 수강 목록': [],
+      };
+
+      console.log(classInfoData.value);
+      classInfoData.value.forEach((item) => {
+        if (item.startDate === yesterdayString) {
+          groupedData['전일 수강 목록'].push(item);
+        } else if (item.startDate === today) {
+          groupedData['오늘 수강 목록'].push(item);
+        } else if (item.startDate === tomorrowString) {
+          groupedData['내일 수강 목록'].push(item);
+        }
+      });
+
+      return groupedData;
+    });
+
     return {
+      filteredClassInfoData,
       classInfoData,
     };
   },
