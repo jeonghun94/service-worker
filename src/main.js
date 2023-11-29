0663bb4dcf072d8611fbcccf83a46cd5590ac06d
jeonghun94 <jeongh1021@naver.com>
2023-11-29 15:41:56 +0900

    main.js 리팩토링

--- src/main.js
+++ src/main.js
@@ -11,26 +11,14 @@ import useNetworkStore from './stores/network';
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
-let isOnline = navigator.onLine;
-
-// 온/오프라인 이벤트 핸들러
-const handleOnline = () => {
-  isOnline = true;
-  // 필요한 경우 스토어의 상태 업데이트
+const handleNetworkChange = () => {
   const { setIsOnline } = useNetworkStore(pinia);
+  const isOnline = navigator.onLine;
   setIsOnline(isOnline);
 };
 
-const handleOffline = () => {
-  isOnline = false;
-  // 필요한 경우 스토어의 상태 업데이트
-  const { setIsOnline } = useNetworkStore(pinia);
-  setIsOnline(isOnline);
-};
-
-// 온/오프라인 이벤트 리스너 등록
-window.addEventListener('online', handleOnline);
-window.addEventListener('offline', handleOffline);
+window.addEventListener('online', handleNetworkChange);
+window.addEventListener('offline', handleNetworkChange);
 
 /* eslint-disable */
 Kakao.init('21a3ba5a34d345b07bbad4098bb4619f');
